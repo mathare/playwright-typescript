975c8d6ce56bcd96a16e3d448e2e411ff6743d58
Mat Hare <mat_hare@yahoo.com>
2024-04-11 10:57:23 +0100

    Add missing test data

---
+++ tests/data/signInPage.ts
@@ -0,0 +1,13 @@
+export const ExpectedText = {
+  PageTitle: 'Customer Login',
+  ExistingCustomerHeading: 'Registered Customers',
+  ExistingCustomerSubheading: 'If you have an account, sign in with your email address.',
+  EmailInputLabel: 'Email',
+  PasswordInputLabel: 'Password',
+  SignInButton: 'Sign In',
+  ForgottenPasswordLink: 'Forgot Your Password?',
+  NewCustomerHeading: 'New Customers',
+  NewCustomerContent:
+    'Creating an account has many benefits: check out faster, keep more than one address, track orders and more.',
+  CreateAccountButton: 'Create an Account',
+};
