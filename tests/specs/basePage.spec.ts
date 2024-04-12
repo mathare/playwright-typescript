7df703b38207b992ffd0bc5a28e24a23b42567b2
Mat Hare <mat_hare@yahoo.com>
2024-04-12 10:23:20 +0100

    BasePage - soft assertions

--- tests/specs/basePage.spec.ts
+++ tests/specs/basePage.spec.ts
@@ -16,60 +16,60 @@ test.describe('Base page tests', () => {
     // The tests could be combined but I have split them here to make them easier to read and maintain
 
     test('Common page elements displayed', async () => {
-      await expect(basePage.globalMessage).toBeVisible();
-      await expect(basePage.pageHeader).toBeVisible();
-      await expect(basePage.topNav).toBeVisible();
-      await expect(basePage.pageFooter).toBeVisible();
-      await expect(basePage.copyrightFooter).toBeVisible();
+      await expect.soft(basePage.globalMessage).toBeVisible();
+      await expect.soft(basePage.pageHeader).toBeVisible();
+      await expect.soft(basePage.topNav).toBeVisible();
+      await expect.soft(basePage.pageFooter).toBeVisible();
+      await expect.soft(basePage.copyrightFooter).toBeVisible();
     });
 
     test('Element styling', async () => {
-      await expect(basePage.globalMessage).toHaveCSS('background-color', Colors.Red);
-      await expect(basePage.globalMessage).toHaveCSS('color', Colors.White);
-      await expect(basePage.globalMessage).toHaveCSS('font-size', GlobalMessageStyle.FontSize);
-      await expect(basePage.globalMessage).toHaveCSS('font-weight', GlobalMessageStyle.FontWeight);
-      await expect(basePage.banner).toHaveCSS('background-color', Colors.Grey);
-      await expect(basePage.banner).toHaveCSS('color', Colors.White);
-      await expect(basePage.topNav).toHaveCSS('background-color', Colors.LightGrey);
-      await expect(basePage.topNav).toHaveCSS('color', Colors.DarkGrey);
-      await expect(basePage.pageFooter).toHaveCSS('background-color', Colors.LighterGrey);
-      await expect(basePage.pageFooter).toHaveCSS('color', Colors.DarkGrey);
-      await expect(basePage.copyrightFooter).toHaveCSS('background-color', Colors.Grey);
-      await expect(basePage.copyrightFooter).toHaveCSS('color', Colors.White);
+      await expect.soft(basePage.globalMessage).toHaveCSS('background-color', Colors.Red);
+      await expect.soft(basePage.globalMessage).toHaveCSS('color', Colors.White);
+      await expect.soft(basePage.globalMessage).toHaveCSS('font-size', GlobalMessageStyle.FontSize);
+      await expect.soft(basePage.globalMessage).toHaveCSS('font-weight', GlobalMessageStyle.FontWeight);
+      await expect.soft(basePage.banner).toHaveCSS('background-color', Colors.Grey);
+      await expect.soft(basePage.banner).toHaveCSS('color', Colors.White);
+      await expect.soft(basePage.topNav).toHaveCSS('background-color', Colors.LightGrey);
+      await expect.soft(basePage.topNav).toHaveCSS('color', Colors.DarkGrey);
+      await expect.soft(basePage.pageFooter).toHaveCSS('background-color', Colors.LighterGrey);
+      await expect.soft(basePage.pageFooter).toHaveCSS('color', Colors.DarkGrey);
+      await expect.soft(basePage.copyrightFooter).toHaveCSS('background-color', Colors.Grey);
+      await expect.soft(basePage.copyrightFooter).toHaveCSS('color', Colors.White);
     });
 
     test('Text content of page elements', async () => {
-      await expect(basePage.globalMessage).toHaveText(ExpectedText.GlobalMessage);
-      await expect(basePage.banner).toHaveText(ExpectedText.Banner);
-      await expect(basePage.searchInput).toBeEmpty();
-      await expect(basePage.searchInput).toHaveAttribute('placeholder', ExpectedText.Search);
+      await expect.soft(basePage.globalMessage).toHaveText(ExpectedText.GlobalMessage);
+      await expect.soft(basePage.banner).toHaveText(ExpectedText.Banner);
+      await expect.soft(basePage.searchInput).toBeEmpty();
+      await expect.soft(basePage.searchInput).toHaveAttribute('placeholder', ExpectedText.Search);
       const topNavLinks = basePage.topNavLvl0Link;
       for (let i = 0; i < (await topNavLinks.count()); i++) {
-        await expect(topNavLinks.nth(i)).toHaveText(ExpectedText.TopNav[i]);
+        await expect.soft(topNavLinks.nth(i)).toHaveText(ExpectedText.TopNav[i]);
       }
       const footerLinks = basePage.pageFooterLink;
       for (let i = 0; i < (await footerLinks.count()); i++) {
-        await expect(footerLinks.nth(i)).toHaveText(ExpectedText.FooterLinks[i]);
+        await expect.soft(footerLinks.nth(i)).toHaveText(ExpectedText.FooterLinks[i]);
       }
-      await expect(basePage.copyrightFooter).toHaveText(ExpectedText.Copyright);
+      await expect.soft(basePage.copyrightFooter).toHaveText(ExpectedText.Copyright);
     });
   });
 
   test.describe('Link tests', () => {
     test('Banner links', async ({ baseURL }) => {
-      await expect(basePage.bannerLink.first()).toHaveAttribute('href', new RegExp(`${baseURL}${Links.SignIn}`));
-      await expect(basePage.bannerLink.nth(1)).toHaveAttribute(
+      await expect.soft(basePage.bannerLink.first()).toHaveAttribute('href', new RegExp(`${baseURL}${Links.SignIn}`));
+      await expect.soft(basePage.bannerLink.nth(1)).toHaveAttribute(
         'href',
         new RegExp(`${baseURL}${Links.CreateAnAccount}`),
       );
     });
 
     test('Logo link', async ({ baseURL }) => {
-      await expect(basePage.logoLink).toHaveAttribute('href', `${baseURL}${Links.Logo}`);
+      await expect.soft(basePage.logoLink).toHaveAttribute('href', `${baseURL}${Links.Logo}`);
     });
 
     test('Shopping cart link', async ({ baseURL }) => {
-      await expect(basePage.cartLink).toHaveAttribute('href', `${baseURL}${Links.Cart}`);
+      await expect.soft(basePage.cartLink).toHaveAttribute('href', `${baseURL}${Links.Cart}`);
     });
 
     test('TopNav links', async ({ baseURL }) => {
@@ -77,7 +77,7 @@ test.describe('Base page tests', () => {
       expect(await lvl0Links.count()).toBeGreaterThan(0);
       for (let i = 0; i < (await lvl0Links.count()); i++) {
         const lvl0Text = (await lvl0Links.nth(i).innerText()).replace(/\W+/g, '');
-        await expect(lvl0Links.nth(i)).toHaveAttribute('href', `${baseURL}${Links.TopNav[lvl0Text]}`);
+        await expect.soft(lvl0Links.nth(i)).toHaveAttribute('href', `${baseURL}${Links.TopNav[lvl0Text]}`);
 
         // This section of the test isn't as neat as I would have liked. I wanted to verify the submenu levels individually e.g Women has Tops and Bottoms as level 1 menu items each with individual submenus. However, the DOM makes that awkward and as I am using a 3rd-party website I can't edit the DOM to add suitable locators/attributes as I would with a website under my control
         if (lvl0Text !== 'WhatsNew' && lvl0Text !== 'Sale') {
@@ -85,7 +85,7 @@ test.describe('Base page tests', () => {
           expect(await subMenuLinks.count()).toBeGreaterThan(0);
           for (let j = 0; j < (await subMenuLinks.count()); j++) {
             const subMenuText = (await subMenuLinks.nth(j).innerText()).replace(/\W+/g, '');
-            await expect(subMenuLinks.nth(j)).toHaveAttribute(
+            await expect.soft(subMenuLinks.nth(j)).toHaveAttribute(
               'href',
               `${baseURL}${Links.TopNav[`${lvl0Text}SubMenu`][subMenuText]}`,
             );
@@ -96,11 +96,12 @@ test.describe('Base page tests', () => {
 
     test('Footer links', async ({ baseURL }) => {
       const footerLinks = basePage.pageFooterLink;
+      expect(await footerLinks.count()).toBeGreaterThan(0);
       // The first link in the footer has a different base URL to the others so test it separately
-      await expect(footerLinks.nth(0)).toHaveAttribute('href', `${Links.Footer['Notes']}`);
+      await expect.soft(footerLinks.nth(0)).toHaveAttribute('href', `${Links.Footer['Notes']}`);
       for (let i = 1; i < (await footerLinks.count()); i++) {
         const linkText = (await footerLinks.nth(i).innerText()).replace(/\W+/g, '').replace('and', '');
-        await expect(footerLinks.nth(i)).toHaveAttribute('href', `${baseURL}${Links.Footer[linkText]}`);
+        await expect.soft(footerLinks.nth(i)).toHaveAttribute('href', `${baseURL}${Links.Footer[linkText]}`);
       }
     });
   });
