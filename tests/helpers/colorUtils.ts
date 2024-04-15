81191abafa187f7b56ac9acedcd3309d8cb5f64d
Mat Hare <mat_hare@yahoo.com>
2024-04-15 12:06:51 +0100

    Linting

--- tests/helpers/colorUtils.ts
+++ tests/helpers/colorUtils.ts
@@ -1,9 +1,9 @@
-export function rgbToHex(rgb: string):string {
-  const colorVals = rgb.slice(4, rgb.length - 1).split(',')
-  let hexStr = '#'
-  colorVals.forEach(val => {
-    const hexVal = parseInt(val, 10).toString(16)
-    hexStr+=hexVal.length===1?`0${hexVal}`:hexVal
-  })
-  return hexStr
+export function rgbToHex(rgb: string): string {
+  const colorVals = rgb.slice(4, rgb.length - 1).split(',');
+  let hexStr = '#';
+  colorVals.forEach((val) => {
+    const hexVal = parseInt(val, 10).toString(16);
+    hexStr += hexVal.length === 1 ? `0${hexVal}` : hexVal;
+  });
+  return hexStr;
 }
--- tests/specs/homePage.spec.ts
+++ tests/specs/homePage.spec.ts
@@ -14,7 +14,7 @@ test.describe('Home page tests', () => {
   test.describe('Appearance tests', () => {
     // This is an example of performing visual-style testing without actual using image comparison but asserting against various element properties
     // The tests could be combined but I have split them here to make them easier to read and maintain
-    const swatchSelectedClass = /selected/
+    const swatchSelectedClass = /selected/;
 
     test('Main page elements displayed', async () => {
       await expect.soft(homePage.globalMessage).toBeVisible();
@@ -47,22 +47,18 @@ test.describe('Home page tests', () => {
       const productItems = homePage.productItem;
       expect(await productItems.count()).toBeGreaterThan(0);
       for (let i = 0; i < (await productItems.count()); i++) {
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Name)).toHaveText(
-          Products[i].title,
-        );
+        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Name)).toHaveText(Products[i].title);
         if (Products[i].rating) {
-          await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Rating)).toHaveText(
-            Products[i].rating!,
-          );
+          await expect
+            .soft(homePage.getProductItemElement(i, ProductItemElements.Rating))
+            .toHaveText(Products[i].rating!);
         }
         if (Products[i].reviews) {
-          await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Reviews)).toHaveText(
-            Products[i].reviews!,
-          );
+          await expect
+            .soft(homePage.getProductItemElement(i, ProductItemElements.Reviews))
+            .toHaveText(Products[i].reviews!);
         }
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Price)).toHaveText(
-          Products[i].price,
-        );
+        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.Price)).toHaveText(Products[i].price);
         if (Products[i].sizes) {
           const sizes = homePage.getProductItemElement(i, ProductItemElements.Sizes);
           expect(await sizes.count()).toBeGreaterThan(0);
@@ -77,50 +73,50 @@ test.describe('Home page tests', () => {
             await expect.soft(colors.nth(j)).toHaveCSS('background-color', Products[i].colors![j]);
           }
         }
-        await productItems.nth(i).hover()
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToCartButton)).toBeVisible()
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToWishListButton)).toBeVisible()
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToCompareButton)).toBeVisible()
+        await productItems.nth(i).hover();
+        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToCartButton)).toBeVisible();
+        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToWishListButton)).toBeVisible();
+        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.AddToCompareButton)).toBeVisible();
       }
     });
 
     test('Product swatch styling', async () => {
       // There is no need to test the swatches on all product items since they all use the same element
-      // Similarly there is no need to test every size/colour swatch for a given product     
-      const sizes = homePage.getProductItemElement(0, ProductItemElements.Sizes)
-     
+      // Similarly there is no need to test every size/colour swatch for a given product
+      const sizes = homePage.getProductItemElement(0, ProductItemElements.Sizes);
+
       // Select
-      await sizes.first().click()
-      await expect.soft(sizes.first()).toHaveClass(swatchSelectedClass)
-      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Hovered)
-      await expect.soft(sizes.first()).toHaveCSS('background-color', Colors.White)
+      await sizes.first().click();
+      await expect.soft(sizes.first()).toHaveClass(swatchSelectedClass);
+      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Hovered);
+      await expect.soft(sizes.first()).toHaveCSS('background-color', Colors.White);
       // For some reason a simple blur() isn't enough so hover over another element
-      await homePage.getProductItemElement(0, ProductItemElements.Name).hover()
-      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Selected)
+      await homePage.getProductItemElement(0, ProductItemElements.Name).hover();
+      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Selected);
 
       //Deselect
-      await sizes.first().click()
-      await expect.soft(sizes.first()).not.toHaveClass(swatchSelectedClass)
-      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Hovered)
-      await expect.soft(sizes.first()).toHaveCSS('background-color', Colors.LightGrey)
-      await homePage.getProductItemElement(0, ProductItemElements.Name).hover()
-      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.NotSelected)
+      await sizes.first().click();
+      await expect.soft(sizes.first()).not.toHaveClass(swatchSelectedClass);
+      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.Hovered);
+      await expect.soft(sizes.first()).toHaveCSS('background-color', Colors.LightGrey);
+      await homePage.getProductItemElement(0, ProductItemElements.Name).hover();
+      await expect.soft(sizes.first()).toHaveCSS('outline', SwatchOutlineStyles.Sizes.NotSelected);
 
-      const colors = homePage.getProductItemElement(0, ProductItemElements.Colors)
-      
+      const colors = homePage.getProductItemElement(0, ProductItemElements.Colors);
+
       // Select
-      await colors.first().click()
-      await expect.soft(colors.first()).toHaveClass(swatchSelectedClass)
-      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Hovered)
+      await colors.first().click();
+      await expect.soft(colors.first()).toHaveClass(swatchSelectedClass);
+      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Hovered);
       // For some reason a simple blur() isn't enough so hover over another element
-      await homePage.getProductItemElement(0, ProductItemElements.Name).hover()
-      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Selected)
+      await homePage.getProductItemElement(0, ProductItemElements.Name).hover();
+      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Selected);
 
       //Deselect
-      await colors.first().click()
-      await expect.soft(colors.first()).not.toHaveClass(swatchSelectedClass)
-      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Hovered)
-    })
+      await colors.first().click();
+      await expect.soft(colors.first()).not.toHaveClass(swatchSelectedClass);
+      await expect.soft(colors.first()).toHaveCSS('outline', SwatchOutlineStyles.Colors.Hovered);
+    });
 
     test('Can only select single size option at a time', async () => {
       // The styling of the 'selected' class is tested above so just checking whether an element has the class is sufficient here
@@ -154,7 +150,7 @@ test.describe('Home page tests', () => {
   });
 
   test.describe('Visual tests', () => {
-    test('Default page appearance', async ({page}) => {
+    test('Default page appearance', async ({ page }) => {
       await expect(page).toHaveScreenshot('default.png', {
         fullPage: true,
         mask: [homePage.adsWidget],
@@ -163,56 +159,62 @@ test.describe('Home page tests', () => {
     });
 
     test('Product hover', async () => {
-      await homePage.productItem.nth(0).hover()
-      await(expect(homePage.productsGrid)).toHaveScreenshot('productHover.png', {timeout: 20000})
-    })
-  })
+      await homePage.productItem.nth(0).hover();
+      await expect(homePage.productsGrid).toHaveScreenshot('productHover.png', { timeout: 20000 });
+    });
+  });
 
-  test.describe('Link tests', () => { 
+  test.describe('Link tests', () => {
     test('Promo block links', async ({ baseURL }) => {
-      const promoBlocks = homePage.promoBlock
-      expect(await promoBlocks.count()).toBeGreaterThan(0)
-      for (let i = 0; i < await promoBlocks.count(); i++) {
+      const promoBlocks = homePage.promoBlock;
+      expect(await promoBlocks.count()).toBeGreaterThan(0);
+      for (let i = 0; i < (await promoBlocks.count()); i++) {
         await expect.soft(promoBlocks.nth(i)).toHaveAttribute('href', `${baseURL}${PromoBlockLinks[i]}`);
       }
-    })
+    });
 
     test('Product links', async ({ baseURL }) => {
-      const products = homePage.productItem
-      expect(await products.count()).toBeGreaterThan(0)
-      for (let i = 0; i < await products.count(); i++) {
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.PhotoLink)).toHaveAttribute('href', `${baseURL}${Products[i].link}`);
-        await expect.soft(homePage.getProductItemElement(i, ProductItemElements.NameLink)).toHaveAttribute('href', `${baseURL}${Products[i].link}`);
+      const products = homePage.productItem;
+      expect(await products.count()).toBeGreaterThan(0);
+      for (let i = 0; i < (await products.count()); i++) {
+        await expect
+          .soft(homePage.getProductItemElement(i, ProductItemElements.PhotoLink))
+          .toHaveAttribute('href', `${baseURL}${Products[i].link}`);
+        await expect
+          .soft(homePage.getProductItemElement(i, ProductItemElements.NameLink))
+          .toHaveAttribute('href', `${baseURL}${Products[i].link}`);
         if (Products[i].reviews) {
-          await expect.soft(homePage.getProductItemElement(i, ProductItemElements.ReviewsLink)).toHaveAttribute('href', `${baseURL}${Products[i].link}#reviews`);
+          await expect
+            .soft(homePage.getProductItemElement(i, ProductItemElements.ReviewsLink))
+            .toHaveAttribute('href', `${baseURL}${Products[i].link}#reviews`);
         } else {
-          await expect.soft(homePage.getProductItemElement(i, ProductItemElements.ReviewsLink)).not.toBeVisible()
+          await expect.soft(homePage.getProductItemElement(i, ProductItemElements.ReviewsLink)).not.toBeVisible();
         }
       }
-    })
-  })
+    });
+  });
 
   test.describe('Tooltip tests', () => {
-    const tooltipWidth = '110'
-      const tooltipHeight = '90'
-    test('Size option tooltips', async () => {      
+    const tooltipWidth = '110';
+    const tooltipHeight = '90';
+    test('Size option tooltips', async () => {
       const sizes = homePage.getProductItemElement(0, ProductItemElements.Sizes);
       expect(await sizes.count()).toBeGreaterThan(0);
       for (let i = 0; i < (await sizes.count()); i++) {
-        await expect.soft(sizes.nth(i)).toHaveAttribute('option-tooltip-value', Products[0].sizes![i]) 
-        await expect.soft(sizes.nth(i)).toHaveAttribute('thumb-width', tooltipWidth) 
-        await expect.soft(sizes.nth(i)).toHaveAttribute('thumb-height', tooltipHeight)         
+        await expect.soft(sizes.nth(i)).toHaveAttribute('option-tooltip-value', Products[0].sizes![i]);
+        await expect.soft(sizes.nth(i)).toHaveAttribute('thumb-width', tooltipWidth);
+        await expect.soft(sizes.nth(i)).toHaveAttribute('thumb-height', tooltipHeight);
       }
-    })
+    });
 
     test('Color swatch tooltips', async () => {
       const colors = homePage.getProductItemElement(0, ProductItemElements.Colors);
       expect(await colors.count()).toBeGreaterThan(0);
       for (let i = 0; i < (await colors.count()); i++) {
-        await expect.soft(colors.nth(i)).toHaveAttribute('option-tooltip-value', rgbToHex(Products[0].colors![i])) 
-        await expect.soft(colors.nth(i)).toHaveAttribute('thumb-width', tooltipWidth) 
-        await expect.soft(colors.nth(i)).toHaveAttribute('thumb-height', tooltipHeight)         
+        await expect.soft(colors.nth(i)).toHaveAttribute('option-tooltip-value', rgbToHex(Products[0].colors![i]));
+        await expect.soft(colors.nth(i)).toHaveAttribute('thumb-width', tooltipWidth);
+        await expect.soft(colors.nth(i)).toHaveAttribute('thumb-height', tooltipHeight);
       }
-    })
-  })
+    });
+  });
 });
