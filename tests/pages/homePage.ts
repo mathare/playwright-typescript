2cd424d69fd2f3dc57cbc5f9be59b2f2faf08694
Mat Hare <mat_hare@yahoo.com>
2024-04-16 10:22:14 +0100

    Add missing locator

--- tests/pages/homePage.ts
+++ tests/pages/homePage.ts
@@ -35,6 +35,7 @@ export enum ProductItemElements {
   Sizes = '.swatch-option.text',
   Colors = '.swatch-option.color',
   PhotoLink = 'a.product-item-photo',
+  Photo = `${PhotoLink} img`,
   NameLink = 'a.product-item-link',
   ReviewsLink = `${Reviews} a`,
   AddToCartButton = 'button.action.tocart',
