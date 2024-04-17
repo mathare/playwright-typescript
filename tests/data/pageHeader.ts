ff57130d1c64fa94e9e43c355be536bdb167aae3
Mat Hare <mat_hare@yahoo.com>
2024-04-17 18:16:28 +0100

    Page header tests

---
+++ tests/data/pageHeader.ts
@@ -0,0 +1,68 @@
+export const ExpectedText = {
+  Banner: 'Skip to Content Click “Write for us” link in the footer to submit a guest post Sign In Create an Account',
+  Search: 'Search entire store here...',
+  Topnav: ["What's New", 'Women', 'Men', 'Gear', 'Training', 'Sale'],
+};
+
+const TopnavLvl0 = {
+  WhatsNew: '/what-is-new.html',
+  Women: '/women.html',
+  Men: '/men.html',
+  Gear: '/gear.html',
+  Training: '/training.html',
+  Sale: '/sale.html',
+};
+
+const WomenSubMenu = {
+  Tops: '/women/tops-women.html',
+  Jackets: '/women/tops-women/jackets-women.html',
+  HoodiesSweatshirts: '/women/tops-women/hoodies-and-sweatshirts-women.html',
+  Tees: '/women/tops-women/tees-women.html',
+  BrasTanks: '/women/tops-women/tanks-women.html',
+  Bottoms: '/women/bottoms-women.html',
+  Pants: '/women/bottoms-women/pants-women.html',
+  Shorts: '/women/bottoms-women/shorts-women.html',
+};
+
+const MenSubMenu = {
+  Tops: '/men/tops-men.html',
+  Jackets: '/men/tops-men/jackets-men.html',
+  HoodiesSweatshirts: '/men/tops-men/hoodies-and-sweatshirts-men.html',
+  Tees: '/men/tops-men/tees-men.html',
+  Tanks: '/men/tops-men/tanks-men.html',
+  Bottoms: '/men/bottoms-men.html',
+  Pants: '/men/bottoms-men/pants-men.html',
+  Shorts: '/men/bottoms-men/shorts-men.html',
+};
+
+const GearSubMenu = {
+  Bags: '/gear/bags.html',
+  FitnessEquipment: '/gear/fitness-equipment.html',
+  Watches: '/gear/watches.html',
+};
+
+const TrainingSubMenu = {
+  VideoDownload: '/training/training-video.html',
+};
+
+export const Links = {
+  SignIn: '/customer/account/login/referer/*/',
+  CreateAnAccount: '/customer/account/create/',
+  Logo: '/',
+  Cart: '/checkout/cart/',
+  Topnav: {
+    ...TopnavLvl0,
+    WomenSubMenu: {
+      ...WomenSubMenu,
+    },
+    MenSubMenu: {
+      ...MenSubMenu,
+    },
+    GearSubMenu: {
+      ...GearSubMenu,
+    },
+    TrainingSubMenu: {
+      ...TrainingSubMenu,
+    },
+  },
+};
