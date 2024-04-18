95d03defbfeb883ea385dff554b904993c79bec5
Mat Hare <mat_hare@yahoo.com>
2024-04-18 15:46:03 +0100

    Move base page colours

--- tests/data/basePage.ts
+++ tests/data/basePage.ts
@@ -3,6 +3,11 @@ export const ExpectedText = {
     'This is a demo store to test your test automaiton scripts. No orders will be fulfilled. If you are facing any issue, email us at hello@softwaretestingboard.com.',
 };
 
+export const Colors = {
+  Red: 'rgb(255, 1, 1)',
+  White: 'rgb(255, 255, 255)',
+};
+
 export const enum GlobalMessageStyle {
   FontSize = '13px',
   FontWeight = '400',
