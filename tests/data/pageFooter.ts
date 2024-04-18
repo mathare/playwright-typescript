544080de9f8221d481ba79a6970a2723452d1081
Mat Hare <mat_hare@yahoo.com>
2024-04-18 10:55:20 +0100

    Page footer tests

---
+++ tests/data/pageFooter.ts
@@ -0,0 +1,25 @@
+export const ExpectedText = {
+  FooterLinks: [
+    'Notes',
+    'Practice API Testing using Magento 2',
+    'Write for us',
+    'Subscribe',
+    'Search Terms',
+    'Privacy and Cookie Policy',
+    'Advanced Search',
+    'Orders and Returns',
+  ],
+  Copyright:
+    'We know you have an assignment to complete. If this site is not functioning as expected, drop us an email. Copyright © 2013-present Magento, Inc. All rights reserved.',
+};
+
+export const FooterLinks = [
+  'https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion',
+  'https://softwaretestingboard.com/practice-api-testing-using-magento-2/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=API_Testing_Promo',
+  'https://softwaretestingboard.com/write-for-us/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=write4us',
+  'https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe',
+  `/search/term/popular/`,
+  `/privacy-policy-cookie-restriction-mode/`,
+  `/catalogsearch/advanced/`,
+  `/sales/guest/form/`,
+];
