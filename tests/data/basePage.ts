6568e275af271f3b27a16dc88c29c208c5309b7f
Mat Hare <mat_hare@yahoo.com>
2024-04-18 10:56:47 +0100

    Update BasePage tests

--- tests/data/basePage.ts
+++ tests/data/basePage.ts
@@ -1,97 +1,9 @@
 export const ExpectedText = {
   GlobalMessage:
     'This is a demo store to test your test automaiton scripts. No orders will be fulfilled. If you are facing any issue, email us at hello@softwaretestingboard.com.',
-  Banner: 'Skip to Content Click “Write for us” link in the footer to submit a guest post Sign In Create an Account',
-  Search: 'Search entire store here...',
-  Topnav: ["What's New", 'Women', 'Men', 'Gear', 'Training', 'Sale'],
-  FooterLinks: [
-    'Notes',
-    'Practice API Testing using Magento 2',
-    'Write for us',
-    'Subscribe',
-    'Search Terms',
-    'Privacy and Cookie Policy',
-    'Advanced Search',
-    'Orders and Returns',
-  ],
-  Copyright:
-    'We know you have an assignment to complete. If this site is not functioning as expected, drop us an email. Copyright © 2013-present Magento, Inc. All rights reserved.',
 };
 
 export const enum GlobalMessageStyle {
   FontSize = '13px',
   FontWeight = '400',
 }
-
-const TopnavLvl0 = {
-  WhatsNew: '/what-is-new.html',
-  Women: '/women.html',
-  Men: '/men.html',
-  Gear: '/gear.html',
-  Training: '/training.html',
-  Sale: '/sale.html',
-};
-
-const WomenSubMenu = {
-  Tops: '/women/tops-women.html',
-  Jackets: '/women/tops-women/jackets-women.html',
-  HoodiesSweatshirts: '/women/tops-women/hoodies-and-sweatshirts-women.html',
-  Tees: '/women/tops-women/tees-women.html',
-  BrasTanks: '/women/tops-women/tanks-women.html',
-  Bottoms: '/women/bottoms-women.html',
-  Pants: '/women/bottoms-women/pants-women.html',
-  Shorts: '/women/bottoms-women/shorts-women.html',
-};
-
-const MenSubMenu = {
-  Tops: '/men/tops-men.html',
-  Jackets: '/men/tops-men/jackets-men.html',
-  HoodiesSweatshirts: '/men/tops-men/hoodies-and-sweatshirts-men.html',
-  Tees: '/men/tops-men/tees-men.html',
-  Tanks: '/men/tops-men/tanks-men.html',
-  Bottoms: '/men/bottoms-men.html',
-  Pants: '/men/bottoms-men/pants-men.html',
-  Shorts: '/men/bottoms-men/shorts-men.html',
-};
-
-const GearSubMenu = {
-  Bags: '/gear/bags.html',
-  FitnessEquipment: '/gear/fitness-equipment.html',
-  Watches: '/gear/watches.html',
-};
-
-const TrainingSubMenu = {
-  VideoDownload: '/training/training-video.html',
-};
-
-export const Links = {
-  SignIn: '/customer/account/login/referer/*/',
-  CreateAnAccount: '/customer/account/create/',
-  Logo: '/',
-  Cart: '/checkout/cart/',
-  Topnav: {
-    ...TopnavLvl0,
-    WomenSubMenu: {
-      ...WomenSubMenu,
-    },
-    MenSubMenu: {
-      ...MenSubMenu,
-    },
-    GearSubMenu: {
-      ...GearSubMenu,
-    },
-    TrainingSubMenu: {
-      ...TrainingSubMenu,
-    },
-  },
-  Footer: [
-    'https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion',
-    'https://softwaretestingboard.com/practice-api-testing-using-magento-2/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=API_Testing_Promo',
-    'https://softwaretestingboard.com/write-for-us/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=write4us',
-    'https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe',
-    `/search/term/popular/`,
-    `/privacy-policy-cookie-restriction-mode/`,
-    `/catalogsearch/advanced/`,
-    `/sales/guest/form/`,
-  ],
-};
--- tests/specs/basePage.spec.ts
+++ tests/specs/basePage.spec.ts
@@ -1,6 +1,5 @@
-import { ExpectedText, GlobalMessageStyle, Links } from '../data/basePage';
+import { ExpectedText, GlobalMessageStyle } from '../data/basePage';
 import { Colors } from '../data/shared';
-import { elementCount } from '../helpers/elementUtils';
 import BasePage from '../pages/basePage';
 import { test, expect } from '@playwright/test';
 
@@ -18,10 +17,10 @@ test.describe('Base page tests', () => {
 
     test('Common page elements displayed', async () => {
       await expect.soft(basePage.globalMessage).toBeVisible();
-      await expect.soft(basePage.pageHeader).toBeVisible();
-      await expect.soft(basePage.topnav).toBeVisible();
-      await expect.soft(basePage.pageFooter).toBeVisible();
-      await expect.soft(basePage.copyrightFooter).toBeVisible();
+      await expect.soft(basePage.pageHeader.header).toBeVisible();
+      await expect.soft(basePage.pageHeader.topnav).toBeVisible();
+      await expect.soft(basePage.pageFooter.footer).toBeVisible();
+      await expect.soft(basePage.pageFooter.copyrightFooter).toBeVisible();
     });
 
     test('Element styling', async () => {
@@ -29,74 +28,10 @@ test.describe('Base page tests', () => {
       await expect.soft(basePage.globalMessage).toHaveCSS('color', Colors.White);
       await expect.soft(basePage.globalMessage).toHaveCSS('font-size', GlobalMessageStyle.FontSize);
       await expect.soft(basePage.globalMessage).toHaveCSS('font-weight', GlobalMessageStyle.FontWeight);
-      await expect.soft(basePage.banner).toHaveCSS('background-color', Colors.Grey);
-      await expect.soft(basePage.banner).toHaveCSS('color', Colors.White);
-      await expect.soft(basePage.topnav).toHaveCSS('background-color', Colors.LightGrey);
-      await expect.soft(basePage.topnav).toHaveCSS('color', Colors.DarkGrey);
-      await expect.soft(basePage.pageFooter).toHaveCSS('background-color', Colors.LighterGrey);
-      await expect.soft(basePage.pageFooter).toHaveCSS('color', Colors.DarkGrey);
-      await expect.soft(basePage.copyrightFooter).toHaveCSS('background-color', Colors.Grey);
-      await expect.soft(basePage.copyrightFooter).toHaveCSS('color', Colors.White);
     });
 
     test('Text content of page elements', async () => {
       await expect.soft(basePage.globalMessage).toHaveText(ExpectedText.GlobalMessage);
-      await expect.soft(basePage.banner).toHaveText(ExpectedText.Banner);
-      await expect.soft(basePage.searchInput).toBeEmpty();
-      await expect.soft(basePage.searchInput).toHaveAttribute('placeholder', ExpectedText.Search);
-      const topnavLinks = basePage.topnavLvl0Link;
-      for (let i = 0; i < (await elementCount(topnavLinks)); i++) {
-        await expect.soft(topnavLinks.nth(i)).toHaveText(ExpectedText.Topnav[i]);
-      }
-      const footerLinks = basePage.pageFooterLink;
-      for (let i = 0; i < (await elementCount(footerLinks)); i++) {
-        await expect.soft(footerLinks.nth(i)).toHaveText(ExpectedText.FooterLinks[i]);
-      }
-      await expect.soft(basePage.copyrightFooter).toHaveText(ExpectedText.Copyright);
-    });
-  });
-
-  test.describe('Link tests', () => {
-    test('Banner links', async ({ baseURL }) => {
-      await expect.soft(basePage.bannerLink.first()).toHaveAttribute('href', new RegExp(`${baseURL}${Links.SignIn}`));
-      await expect
-        .soft(basePage.bannerLink.nth(1))
-        .toHaveAttribute('href', new RegExp(`${baseURL}${Links.CreateAnAccount}`));
-    });
-
-    test('Logo link', async ({ baseURL }) => {
-      await expect.soft(basePage.logoLink).toHaveAttribute('href', `${baseURL}${Links.Logo}`);
-    });
-
-    test('Shopping cart link', async ({ baseURL }) => {
-      await expect.soft(basePage.cartLink).toHaveAttribute('href', `${baseURL}${Links.Cart}`);
-    });
-
-    test('Topnav links', async ({ baseURL }) => {
-      const lvl0Links = basePage.topnavLvl0Link;
-      for (let i = 0; i < (await elementCount(lvl0Links)); i++) {
-        const lvl0Text = (await lvl0Links.nth(i).innerText()).replace(/\W+/g, '');
-        await expect.soft(lvl0Links.nth(i)).toHaveAttribute('href', `${baseURL}${Links.Topnav[lvl0Text]}`);
-
-        // This section of the test isn't as neat as I would have liked. I wanted to verify the submenu levels individually e.g Women has Tops and Bottoms as level 1 menu items each with individual submenus. However, the DOM makes that awkward and as I am using a 3rd-party website I can't edit the DOM to add suitable locators/attributes as I would with a website under my control
-        if (lvl0Text !== 'WhatsNew' && lvl0Text !== 'Sale') {
-          const subMenuLinks = await basePage.getTopnavSubMenuLinks(i);
-          for (let j = 0; j < (await elementCount(subMenuLinks)); j++) {
-            const subMenuText = (await subMenuLinks.nth(j).innerText()).replace(/\W+/g, '');
-            await expect
-              .soft(subMenuLinks.nth(j))
-              .toHaveAttribute('href', `${baseURL}${Links.Topnav[`${lvl0Text}SubMenu`][subMenuText]}`);
-          }
-        }
-      }
-    });
-
-    test('Footer links', async ({ baseURL }) => {
-      const footerLinks = basePage.pageFooterLink;
-      for (let i = 0; i < (await elementCount(footerLinks)); i++) {
-        const expectedLink = Links.Footer[i].startsWith('https') ? Links.Footer[i] : `${baseURL}${Links.Footer[i]}`;
-        await expect.soft(footerLinks.nth(i)).toHaveAttribute('href', expectedLink);
-      }
     });
   });
 });
