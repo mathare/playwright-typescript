1ff5856ecb2533dcdda5ecbc079cff60427da9c4
Mat Hare <mat_hare@yahoo.com>
2024-04-19 13:36:58 +0100

    Delete obsolete file

--- tests/helpers/elementUtils.ts
+++
@@ -1,8 +0,0 @@
-import { Locator, expect } from '@playwright/test';
-
-export async function elementCount(locator: Locator, expectedCount: number): Promise<number> {
-  await expect(async () => {
-    expect(await locator.count()).toEqual(expectedCount);
-  }).toPass({ timeout: 10000 });
-  return await locator.count();
-}
