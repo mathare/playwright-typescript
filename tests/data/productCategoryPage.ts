f545f9943e461527abb1944a92feb4f1e0b7a9e3
Mat Hare <mat_hare@yahoo.com>
2024-04-24 14:43:24 +0100

    Simplify imports

--- tests/data/productCategoryPage.ts
+++ tests/data/productCategoryPage.ts
@@ -1,12 +1,12 @@
 import { Links as HeaderLinks } from './pageHeader';
-import { ExpectedText as WomenBottomsText, Links as WomenBottomsLinks } from './productCategories/womenBottoms';
-import { ExpectedText as WomenHoodiesText, Links as WomenHoodiesLinks } from './productCategories/womenHoodies';
-import { ExpectedText as WomenJacketsText, Links as WomenJacketsLinks } from './productCategories/womenJackets';
-import { ExpectedText as WomenPantsText, Links as WomenPantsLinks } from './productCategories/womenPants';
-import { ExpectedText as WomenShortsText, Links as WomenShortsLinks } from './productCategories/womenShorts';
-import { ExpectedText as WomenTanksText, Links as WomenTanksLinks } from './productCategories/womenTanks';
-import { ExpectedText as WomenTeesText, Links as WomenTeesLinks } from './productCategories/womenTees';
-import { ExpectedText as WomenTopsText, Links as WomenTopsLinks } from './productCategories/womenTops';
+import * as WomenBottoms from './productCategories/womenBottoms';
+import * as WomenHoodies from './productCategories/womenHoodies';
+import * as WomenJackets from './productCategories/womenJackets';
+import * as WomenPants from './productCategories/womenPants';
+import * as WomenShorts from './productCategories/womenShorts';
+import * as WomenTanks from './productCategories/womenTanks';
+import * as WomenTees from './productCategories/womenTees';
+import * as WomenTops from './productCategories/womenTops';
 
 export const ProductCategories = {
   Women: { ...HeaderLinks.Topnav.WomenSubMenu },
@@ -17,23 +17,23 @@ export const ProductCategories = {
 
 export const ExpectedText = {
   FiltersTitle: 'Shopping Options',
-  WomenBottoms: { ...WomenBottomsText },
-  WomenBrasTanks: { ...WomenTanksText },
-  WomenHoodiesSweatshirts: { ...WomenHoodiesText },
-  WomenJackets: { ...WomenJacketsText },
-  WomenPants: { ...WomenPantsText },
-  WomenShorts: { ...WomenShortsText },
-  WomenTees: { ...WomenTeesText },
-  WomenTops: { ...WomenTopsText },
+  WomenBottoms: { ...WomenBottoms.ExpectedText },
+  WomenBrasTanks: { ...WomenTanks.ExpectedText },
+  WomenHoodiesSweatshirts: { ...WomenHoodies.ExpectedText },
+  WomenJackets: { ...WomenJackets.ExpectedText },
+  WomenPants: { ...WomenPants.ExpectedText },
+  WomenShorts: { ...WomenShorts.ExpectedText },
+  WomenTees: { ...WomenTees.ExpectedText },
+  WomenTops: { ...WomenTops.ExpectedText },
 };
 
 export const Links = {
-  WomenBottoms: { ...WomenBottomsLinks },
-  WomenBrasTanks: { ...WomenTanksLinks },
-  WomenHoodiesSweatshirts: { ...WomenHoodiesLinks },
-  WomenJackets: { ...WomenJacketsLinks },
-  WomenPants: { ...WomenPantsLinks },
-  WomenShorts: { ...WomenShortsLinks },
-  WomenTees: { ...WomenTeesLinks },
-  WomenTops: { ...WomenTopsLinks },
+  WomenBottoms: { ...WomenBottoms.Links },
+  WomenBrasTanks: { ...WomenTanks.Links },
+  WomenHoodiesSweatshirts: { ...WomenHoodies.Links },
+  WomenJackets: { ...WomenJackets.Links },
+  WomenPants: { ...WomenPants.Links },
+  WomenShorts: { ...WomenShorts.Links },
+  WomenTees: { ...WomenTees.Links },
+  WomenTops: { ...WomenTops.Links },
 };
