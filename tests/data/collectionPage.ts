1a268eff4ca32747730d7dd54736c02d972f775a
Mat Hare <mat_hare@yahoo.com>
2024-04-30 13:06:04 +0100

    Men collection data

--- tests/data/collectionPage.ts
+++ tests/data/collectionPage.ts
@@ -1,22 +1,26 @@
 import { TopnavLvl0 } from './pageHeader';
 import * as WhatsNew from './collections/whatsNew';
 import * as Women from './collections/women';
+import * as Men from './collections/men';
 
 export const Collections = {
   ...TopnavLvl0,
 };
 
 export const ExpectedText = {
+  Men: { ...Men.ExpectedText },
   WhatsNew: { ...WhatsNew.ExpectedText },
   Women: { ...Women.ExpectedText },
 };
 
 export const Links = {
+  Men: { ...Men.Links },
   WhatsNew: { ...WhatsNew.Links },
   Women: { ...Women.Links },
 };
 
 export const Products = {
+  Men: [...Men.Products],
   WhatsNew: [...WhatsNew.Products],
   Women: [...Women.Products],
 };
