c30f93b4aa2b46c964686aac8b7034b98ea449ca
Mat Hare <mat_hare@yahoo.com>
2024-05-06 17:56:47 +0100

    Delete FilterOptions const

--- tests/data/productCategories/shared.ts
+++ tests/data/productCategories/shared.ts
@@ -1,24 +1,8 @@
 export type ProductCategoryExpectedText = {
   Breadcrumbs: string;
   Title: string;
-  Filters: string[];
   ProductCount: string;
 };
-
-export const FilterOptions = [
-  'STYLE',
-  'SIZE',
-  'PRICE',
-  'COLOR',
-  'MATERIAL',
-  'ECO COLLECTION',
-  'PERFORMANCE FABRIC',
-  'ERIN RECOMMENDS',
-  'NEW',
-  'SALE',
-  'PATTERN',
-  'CLIMATE',
-];
 
 export type Filter = {
   title: string;
