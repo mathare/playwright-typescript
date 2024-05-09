3489bdeca68eff09bc716ea488c093d989ff1495
Mat Hare <mat_hare@yahoo.com>
2024-05-09 16:07:37 +0100

    Declare filter type

--- tests/data/collections/shared.ts
+++ tests/data/collections/shared.ts
@@ -4,3 +4,13 @@ export type CollectionExpectedText = {
   PromoBlocks: string[];
   ProductsGrid: Record<string, string>;
 };
+
+export type Filter = {
+  title: string;
+  categories: FilterCategory[];
+};
+
+type FilterCategory = {
+  title: string;
+  link: string;
+};
