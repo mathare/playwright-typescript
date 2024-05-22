d3caf6dccc61cf525f6d5654e2aef4a7409d53eb
Mat Hare <mat_hare@yahoo.com>
2024-05-22 17:42:07 +0100

    Update Women Tanks filters

--- tests/data/productCategoryPage.ts
+++ tests/data/productCategoryPage.ts
@@ -115,7 +115,7 @@ export const Links = {
 
 export const Filters = {
   WomenBottoms: [...WomenBottoms.Filters],
-  // WomenBrasTanks: { ...WomenTanks.Filters },
+  WomenBrasTanks: [...WomenTanks.Filters],
   WomenHoodiesSweatshirts: [...WomenHoodies.Filters],
   WomenJackets: [...WomenJackets.Filters],
   // WomenPants: { ...WomenPants.Filters },
