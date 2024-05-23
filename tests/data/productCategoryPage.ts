4406cda92a7cf48879bcd3f4b6bb86dc7d9abaf4
Mat Hare <mat_hare@yahoo.com>
2024-05-23 10:23:38 +0100

    Update Men Pants filters

--- tests/data/productCategoryPage.ts
+++ tests/data/productCategoryPage.ts
@@ -122,7 +122,6 @@ export const Filters = {
   WomenShorts: [...WomenShorts.Filters],
   WomenTees: [...WomenTees.Filters],
   WomenTops: [...WomenTops.Filters],
-  // MenPants: { ...MenPants.Filters },
   // MenShorts: { ...MenShorts.Filters },
   // MenTanks: { ...MenTanks.Filters },
   // MenTees: { ...MenTees.Filters },
@@ -133,6 +132,7 @@ export const Filters = {
   MenBottoms: [...MenBottoms.Filters],
   MenHoodiesSweatshirts: [...MenHoodies.Filters],
   MenJackets: [...MenJackets.Filters],
+  MenPants: [...MenPants.Filters],
 };
 
 export const Products = {
