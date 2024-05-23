4cb94fba75c006560912242e53ef145211caf42d
Mat Hare <mat_hare@yahoo.com>
2024-05-23 10:15:08 +0100

    Update Men Hoodies filters

--- tests/data/productCategories/menHoodies.ts
+++ tests/data/productCategories/menHoodies.ts
@@ -16,145 +16,194 @@ export const Links = {
   },
 };
 
-export const Filters: Record<string, Filter[]> = {
-  Size: [
-    { title: 'XS', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=166` },
-    { title: 'S', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=167` },
-    { title: 'M', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=168` },
-    { title: 'L', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=169` },
-    { title: 'XL', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=170` },
-  ],
-  Climate: [
-    {
-      title: 'All-Weather',
-      count: 9,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=201`,
-    },
-    { title: 'Cool', count: 11, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=203` },
-    { title: 'Indoor', count: 9, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=204` },
-    { title: 'Spring', count: 11, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=207` },
-    { title: 'Windy', count: 9, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=209` },
-  ],
-  Color: [
-    { title: 'Black', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=49` },
-    { title: 'Blue', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=50` },
-    { title: 'Brown', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=51` },
-    { title: 'Gray', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=52` },
-    { title: 'Green', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=53` },
-    { title: 'Lavender', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=54` },
-    { title: 'Orange', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=56` },
-    { title: 'Purple', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=57` },
-    { title: 'Red', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=58` },
-    { title: 'White', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=59` },
-    { title: 'Yellow', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=60` },
-  ],
-  EcoCollection: [
-    {
-      title: 'Yes',
-      count: 3,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?eco_collection=1`,
-    },
-    {
-      title: 'No',
-      count: 10,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?eco_collection=0`,
-    },
-  ],
-  ErinRecommends: [
-    {
-      title: 'Yes',
-      count: 3,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?erin_recommends=1`,
-    },
-    {
-      title: 'No',
-      count: 10,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?erin_recommends=0`,
-    },
-  ],
-  Material: [
-    {
-      title: 'Cocona® performance fabric',
-      count: 1,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=142`,
-    },
-    { title: 'Fleece', count: 5, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=144` },
-    { title: 'Hemp', count: 1, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=145` },
-    {
-      title: 'LumaTech™',
-      count: 1,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=147`,
-    },
-    { title: 'Nylon', count: 5, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=37` },
-    {
-      title: 'Polyester',
-      count: 5,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=38`,
-    },
-    {
-      title: 'Organic Cotton',
-      count: 4,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=153`,
-    },
-    {
-      title: 'CoolTech™',
-      count: 1,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=155`,
-    },
-    { title: 'Wool', count: 6, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=158` },
-  ],
-  New: [
-    { title: 'Yes', count: 3, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?new=1` },
-    { title: 'No', count: 10, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?new=0` },
-  ],
-  Pattern: [
-    {
-      title: 'Color-Blocked',
-      count: 4,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=194`,
-    },
-    { title: 'Solid', count: 8, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=196` },
-    { title: 'Striped', count: 1, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=198` },
-  ],
-  PerformanceFabric: [
-    {
-      title: 'Yes',
-      count: 3,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?performance_fabric=1`,
-    },
-    {
-      title: 'No',
-      count: 10,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?performance_fabric=0`,
-    },
-  ],
-  Price: [
-    {
-      title: '$40.00 - $49.99',
-      count: 2,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=40-50`,
-    },
-    {
-      title: '$50.00 - $59.99',
-      count: 3,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=50-60`,
-    },
-    {
-      title: '$60.00 - $69.99',
-      count: 6,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=60-70`,
-    },
-    {
-      title: '$70.00 and above',
-      count: 2,
-      link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=70-`,
-    },
-  ],
-  Sale: [
-    { title: 'Yes', count: 3, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?sale=1` },
-    { title: 'No', count: 10, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?sale=0` },
-  ],
-};
+export const Filters = [
+  {
+    title: 'SIZE',
+    options: [
+      { title: 'XS', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=166` },
+      { title: 'S', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=167` },
+      { title: 'M', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=168` },
+      { title: 'L', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=169` },
+      { title: 'XL', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?size=170` },
+    ],
+  },
+  {
+    title: 'CLIMATE',
+    options: [
+      {
+        title: 'All-Weather',
+        count: 9,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=201`,
+      },
+      { title: 'Cool', count: 11, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=203` },
+      {
+        title: 'Indoor',
+        count: 9,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=204`,
+      },
+      {
+        title: 'Spring',
+        count: 11,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=207`,
+      },
+      { title: 'Windy', count: 9, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?climate=209` },
+    ],
+  },
+  {
+    title: 'COLOR',
+    options: [
+      { title: 'Black', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=49` },
+      { title: 'Blue', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=50` },
+      { title: 'Brown', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=51` },
+      { title: 'Gray', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=52` },
+      { title: 'Green', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=53` },
+      { title: 'Lavender', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=54` },
+      { title: 'Orange', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=56` },
+      { title: 'Purple', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=57` },
+      { title: 'Red', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=58` },
+      { title: 'White', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=59` },
+      { title: 'Yellow', link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?color=60` },
+    ],
+  },
+  {
+    title: 'ECO COLLECTION',
+    options: [
+      {
+        title: 'Yes',
+        count: 3,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?eco_collection=1`,
+      },
+      {
+        title: 'No',
+        count: 10,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?eco_collection=0`,
+      },
+    ],
+  },
+  {
+    title: 'ERIN RECOMMENDS',
+    options: [
+      {
+        title: 'Yes',
+        count: 3,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?erin_recommends=1`,
+      },
+      {
+        title: 'No',
+        count: 10,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?erin_recommends=0`,
+      },
+    ],
+  },
+  {
+    title: 'MATERIAL',
+    options: [
+      {
+        title: 'Cocona® performance fabric',
+        count: 1,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=142`,
+      },
+      {
+        title: 'Fleece',
+        count: 5,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=144`,
+      },
+      { title: 'Hemp', count: 1, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=145` },
+      {
+        title: 'LumaTech™',
+        count: 1,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=147`,
+      },
+      { title: 'Nylon', count: 5, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=37` },
+      {
+        title: 'Polyester',
+        count: 5,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=38`,
+      },
+      {
+        title: 'Organic Cotton',
+        count: 4,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=153`,
+      },
+      {
+        title: 'CoolTech™',
+        count: 1,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=155`,
+      },
+      { title: 'Wool', count: 6, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?material=158` },
+    ],
+  },
+  {
+    title: 'NEW',
+    options: [
+      { title: 'Yes', count: 3, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?new=1` },
+      { title: 'No', count: 10, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?new=0` },
+    ],
+  },
+  {
+    title: 'PATTERN',
+    options: [
+      {
+        title: 'Color-Blocked',
+        count: 4,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=194`,
+      },
+      { title: 'Solid', count: 8, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=196` },
+      {
+        title: 'Striped',
+        count: 1,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?pattern=198`,
+      },
+    ],
+  },
+  {
+    title: 'PERFORMANCE FABRIC',
+    options: [
+      {
+        title: 'Yes',
+        count: 3,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?performance_fabric=1`,
+      },
+      {
+        title: 'No',
+        count: 10,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?performance_fabric=0`,
+      },
+    ],
+  },
+  {
+    title: 'PRICE',
+    options: [
+      {
+        title: '$40.00 - $49.99',
+        count: 2,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=40-50`,
+      },
+      {
+        title: '$50.00 - $59.99',
+        count: 3,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=50-60`,
+      },
+      {
+        title: '$60.00 - $69.99',
+        count: 6,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=60-70`,
+      },
+      {
+        title: '$70.00 and above',
+        count: 2,
+        link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?price=70-`,
+      },
+    ],
+  },
+  {
+    title: 'SALE',
+    options: [
+      { title: 'Yes', count: 3, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?sale=1` },
+      { title: 'No', count: 10, link: `${HeaderLinks.Topnav.MenSubMenu.TopsSubMenu.HoodiesSweatshirts}?sale=0` },
+    ],
+  },
+];
 
 export const Products: Product[] = [
   {
