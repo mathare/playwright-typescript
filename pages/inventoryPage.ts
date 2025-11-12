100f6086e18032ec02fec7fad9093e714bf0276f
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-11-12 16:23:42 +0000

    Inventory page tests (#3)
    * Visibility test
    
    * Additional visibility test
    
    * Verify expected text of main elements
    
    * Visual tests (win32)
    
    * Draft product info test
    
    * Rework product info test
    
    * Element style tests
    
    * Product styling test
    
    * Restructure tests
    
    * Swap test order
    
    * Verify sorting of products
    
    * Test adding product to cart
    
    * Update cart assertion
    
    * Add const for num products
    
    * Update product details tests
    
    * More add/remove product tests
    
    * Product links test
    
    * Social media links test
    
    * Product link tests
    
    * Verify URL in beforeEach hook
    
    * Verify active sort option when sort changed
    
    * Shopping cart link test
    
    * Additional visual test
    
    * Update keys
    
    * Refactor button style assertions
    
    * Add Linux snapshots

---
+++ pages/inventoryPage.ts
@@ -0,0 +1,169 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class InventoryPage {
+  readonly url = '/inventory.html';
+  readonly page: Page;
+  readonly body: Locator;
+  readonly headerContainer: Locator;
+  readonly menuButton: Locator;
+  readonly menu: Locator;
+  readonly menuItem: Locator;
+  readonly menuCloseButton: Locator;
+  readonly title: Locator;
+  readonly shoppingCartContainer: Locator;
+  readonly shoppingCartLink: Locator;
+  readonly shoppingCartBadge: Locator;
+  readonly secondaryHeader: Locator;
+  readonly subtitle: Locator;
+  readonly activeSortOption: Locator;
+  readonly sortSelect: Locator;
+  readonly inventoryContainer: Locator;
+  readonly inventoryItem: Locator;
+  readonly footer: Locator;
+  readonly socialMediaItem: Locator;
+  readonly socialMediaLink: Locator;
+  readonly footerCopy: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.body = page.locator('body');
+    this.headerContainer = page.getByTestId('header-container');
+    this.menuButton = this.headerContainer.locator('#react-burger-menu-btn');
+    this.menu = page.locator('div.bm-menu-wrap');
+    this.menuItem = this.menu.locator('a.menu-item');
+    this.menuCloseButton = this.menu.locator('#react-burger-cross-btn');
+    this.title = this.headerContainer.locator('div.app_logo');
+    this.shoppingCartContainer = this.headerContainer.locator('#shopping_cart_container');
+    this.shoppingCartLink = this.shoppingCartContainer.getByTestId('shopping-cart-link');
+    this.shoppingCartBadge = this.shoppingCartLink.getByTestId('shopping-cart-badge');
+    this.secondaryHeader = page.getByTestId('secondary-header');
+    this.subtitle = this.secondaryHeader.getByTestId('title');
+    this.activeSortOption = this.secondaryHeader.getByTestId('active-option');
+    this.sortSelect = this.secondaryHeader.getByTestId('product-sort-container');
+    this.inventoryContainer = page.getByTestId('inventory-container');
+    this.inventoryItem = this.inventoryContainer.getByTestId('inventory-item');
+    this.footer = page.getByTestId('footer');
+    this.socialMediaItem = this.footer.locator('ul.social li');
+    this.socialMediaLink = this.socialMediaItem.locator('a');
+    this.footerCopy = this.footer.getByTestId('footer-copy');
+  }
+
+  // *******
+  // ACTIONS
+  // *******
+  getProductElement(index: number, element: string): Locator {
+    return this.inventoryItem.nth(index).locator(element);
+  }
+
+  async addAllProductsToCart(): Promise<void> {
+    for (let i = 0; i < PRODUCT_INFO.length; i++) {
+      await this.getProductElement(i, PRODUCT_ELEMENTS.button).click();
+    }
+  }
+
+  // **********
+  // ASSERTIONS
+  // **********
+  async verifyCartButtonStyle(productIndex: number, style: 'add' | 'remove'): Promise<void> {
+    const BUTTON_TEXT = style === 'add' ? EXPECTED_TEXT.addToCartButton : EXPECTED_TEXT.removeButton;
+    const BUTTON_COLOR = style === 'add' ? COLORS.addButtonColor : COLORS.removeButtonColor;
+    const BUTTON_CLASS = style === 'add' ? 'btn_primary' : 'btn_secondary';
+    await expect(this.getProductElement(productIndex, PRODUCT_ELEMENTS.button)).toHaveText(BUTTON_TEXT);
+    // await expect(this.getProductElement(productIndex, PRODUCT_ELEMENTS.button)).not.toContainClass('btn_primary');
+    await expect(this.getProductElement(productIndex, PRODUCT_ELEMENTS.button)).toContainClass(BUTTON_CLASS);
+    await expect(this.getProductElement(productIndex, PRODUCT_ELEMENTS.button)).toHaveCSS(
+      'border',
+      `1px solid ${BUTTON_COLOR}`
+    );
+    await expect(this.getProductElement(productIndex, PRODUCT_ELEMENTS.button)).toHaveCSS('color', BUTTON_COLOR);
+  }
+}
+
+export const EXPECTED_TEXT = {
+  title: 'Swag Labs',
+  subtitle: 'Products',
+  sortOptions: ['Name (A to Z)', 'Name (Z to A)', 'Price (low to high)', 'Price (high to low)'],
+  socialMedia: ['Twitter', 'Facebook', 'LinkedIn'],
+  footer: '© 2025 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy',
+  menuItems: ['All Items', 'About', 'Logout', 'Reset App State'],
+  addToCartButton: 'Add to cart',
+  removeButton: 'Remove',
+};
+
+export const COLORS = {
+  backgroundColor: 'rgb(255, 255, 255)',
+  textColor: 'rgb(19, 35, 34)',
+  productBorderColor: 'rgb(237, 237, 237)',
+  productTitleColor: 'rgb(24, 88, 58)',
+  addButtonColor: 'rgb(19, 35, 34)',
+  removeButtonColor: 'rgb(226, 35, 26)',
+  footerBackgroundColor: 'rgb(19, 35, 34)',
+  footerTextColor: 'rgb(255, 255, 255)',
+  socialLinkBackgroundColor: 'rgb()',
+  socialLinkTextColor: 'rgb(0, 0, 238)',
+};
+
+export const PRODUCT_INFO = [
+  {
+    id: 4,
+    title: 'Sauce Labs Backpack',
+    imgSrc: '/static/media/sauce-backpack-1200x1500.0a0b85a385945026062b.jpg',
+    description:
+      'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
+    price: 29.99,
+  },
+  {
+    id: 0,
+    title: 'Sauce Labs Bike Light',
+    imgSrc: '/static/media/bike-light-1200x1500.37c843b09a7d77409d63.jpg',
+    description:
+      "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
+    price: 9.99,
+  },
+  {
+    id: 1,
+    title: 'Sauce Labs Bolt T-Shirt',
+    imgSrc: '/static/media/bolt-shirt-1200x1500.c2599ac5f0a35ed5931e.jpg',
+    description:
+      'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
+    price: 15.99,
+  },
+  {
+    id: 5,
+    title: 'Sauce Labs Fleece Jacket',
+    imgSrc: '/static/media/sauce-pullover-1200x1500.51d7ffaf301e698772c8.jpg',
+    description:
+      "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
+    price: 49.99,
+  },
+  {
+    id: 2,
+    title: 'Sauce Labs Onesie',
+    imgSrc: '/static/media/red-onesie-1200x1500.2ec615b271ef4c3bc430.jpg',
+    description:
+      "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
+    price: 7.99,
+  },
+  {
+    id: 3,
+    title: 'Test.allTheThings() T-Shirt (Red)',
+    imgSrc: '/static/media/red-tatt-1200x1500.30dadef477804e54fc7b.jpg',
+    description:
+      'This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.',
+    price: 15.99,
+  },
+];
+
+export enum PRODUCT_ELEMENTS {
+  img = 'img.inventory_item_img',
+  title = 'div.inventory_item_name',
+  description = 'div.inventory_item_desc',
+  price = 'div.inventory_item_price',
+  button = 'button',
+}
+
+export const SOCIAL_LINKS = [
+  'https://twitter.com/saucelabs',
+  'https://www.facebook.com/saucelabs',
+  'https://www.linkedin.com/company/sauce-labs/',
+];
---
+++ tests/inventoryPage.spec.ts
@@ -0,0 +1,304 @@
+import { test, expect } from '@playwright/test';
+import {
+  COLORS,
+  EXPECTED_TEXT,
+  InventoryPage,
+  PRODUCT_ELEMENTS,
+  PRODUCT_INFO,
+  SOCIAL_LINKS,
+} from '../pages/inventoryPage';
+import { LoginPage } from '../pages/loginPage';
+
+test.describe('Inventory page tests', () => {
+  let inventoryPage: InventoryPage;
+
+  test.beforeEach(async ({ page, baseURL }) => {
+    let loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    await loginPage.login('standard_user');
+    await expect(page).toHaveURL(`${baseURL}${inventoryPage.url}`);
+  });
+
+  test.describe('Appearance tests', () => {
+    test('Default element visibility', async () => {
+      await expect(inventoryPage.headerContainer).toBeVisible();
+      await expect(inventoryPage.menuButton).toBeVisible();
+      await expect(inventoryPage.menu).not.toBeVisible();
+      await expect(inventoryPage.title).toBeVisible();
+      await expect(inventoryPage.shoppingCartLink).toBeVisible();
+      await expect(inventoryPage.shoppingCartBadge).toHaveCount(0);
+      await expect(inventoryPage.secondaryHeader).toBeVisible();
+      await expect(inventoryPage.subtitle).toBeVisible();
+      await expect(inventoryPage.activeSortOption).toBeVisible();
+      await expect(inventoryPage.sortSelect).toBeVisible();
+      await expect(inventoryPage.inventoryContainer).toBeVisible();
+      await expect(inventoryPage.inventoryItem).toHaveCount(PRODUCT_INFO.length);
+      await expect(inventoryPage.footer).toBeVisible();
+      await expect(inventoryPage.socialMediaItem).toHaveCount(EXPECTED_TEXT.socialMedia.length);
+      await expect(inventoryPage.footerCopy).toBeVisible();
+    });
+
+    test('Element visibility with menu open', async () => {
+      await inventoryPage.menuButton.click();
+      await expect(inventoryPage.menu).toBeVisible();
+      await expect(inventoryPage.menuItem).toHaveCount(EXPECTED_TEXT.menuItems.length);
+      await expect(inventoryPage.menuCloseButton).toBeVisible();
+
+      // The menu obscures certain page elements but they still count as visible based on the Playwright definition
+      // Therefore, rather than asserting on the state of the covered elements it is easier to test visually (below)
+    });
+
+    // Verifying the details of each displayed product is done via a separate test
+    test('Text content of elements', async () => {
+      await expect(inventoryPage.title).toHaveText(EXPECTED_TEXT.title);
+      await expect(inventoryPage.subtitle).toHaveText(EXPECTED_TEXT.subtitle);
+      await expect(inventoryPage.activeSortOption).toHaveText(EXPECTED_TEXT.sortOptions[0]);
+      await expect(inventoryPage.sortSelect).toHaveText(EXPECTED_TEXT.sortOptions.join(''));
+      for (let i = 0; i < EXPECTED_TEXT.socialMedia.length; i++) {
+        await expect(inventoryPage.socialMediaItem.nth(i)).toHaveText(EXPECTED_TEXT.socialMedia[i]);
+      }
+      await expect(inventoryPage.footerCopy).toHaveText(EXPECTED_TEXT.footer);
+
+      await inventoryPage.menuButton.click();
+      for (let i = 0; i < EXPECTED_TEXT.menuItems.length; i++) {
+        await expect(inventoryPage.menuItem.nth(i)).toHaveText(EXPECTED_TEXT.menuItems[i]);
+      }
+    });
+
+    test('Element styling', async () => {
+      await expect(inventoryPage.body).toHaveCSS('background-color', COLORS.backgroundColor);
+      await expect(inventoryPage.body).toHaveCSS('color', COLORS.textColor);
+      await expect(inventoryPage.body).toHaveCSS('font-size', '14px');
+      // Menu button is in the top-left
+      await expect(inventoryPage.menuButton).toHaveCSS('position', 'absolute');
+      await expect(inventoryPage.menuButton).toHaveCSS('left', '0px');
+      await expect(inventoryPage.menuButton).toHaveCSS('top', '0px');
+      await expect(inventoryPage.title).toHaveCSS('font-size', '24px');
+      // Shopping cart link is in the top-right (within a container)
+      await expect(inventoryPage.shoppingCartContainer).toHaveCSS('position', 'absolute');
+      await expect(inventoryPage.shoppingCartContainer).toHaveCSS('right', '20px');
+      await expect(inventoryPage.shoppingCartContainer).toHaveCSS('top', '10px');
+      await expect(inventoryPage.shoppingCartContainer).toHaveCSS('width', '40px');
+      await expect(inventoryPage.shoppingCartContainer).toHaveCSS('height', '40px');
+      await expect(inventoryPage.subtitle).toHaveCSS('font-size', '18px');
+      await expect(inventoryPage.subtitle).toHaveCSS('font-weight', '500');
+      await expect(inventoryPage.activeSortOption).toHaveCSS('text-align', 'center');
+
+      await expect(inventoryPage.footer).toHaveCSS('background-color', COLORS.footerBackgroundColor);
+      const numSocialMediaItems = await inventoryPage.socialMediaItem.count();
+      const regex = '^url\\("data:image\\/png;base64,[A-Za-z0-9+\\/=\\"\\)]*$';
+      for (let i = 0; i < numSocialMediaItems; i++) {
+        // Social media items each have a different base64-encoded background image so verify against a regex
+        await expect(inventoryPage.socialMediaItem.nth(i)).toHaveCSS('background-image', new RegExp(regex));
+        await expect(inventoryPage.socialMediaLink.nth(i)).toHaveCSS('color', COLORS.socialLinkTextColor);
+        await expect(inventoryPage.socialMediaLink.nth(i)).toHaveCSS('font-size', '0px');
+      }
+      await expect(inventoryPage.footerCopy).toHaveCSS('color', COLORS.footerTextColor);
+    });
+
+    test.describe('Visual tests', () => {
+      test('Default state', async ({ page }) => {
+        await expect(page).toHaveScreenshot('default.png', { fullPage: true });
+      });
+
+      test('Menu open', async ({ page }) => {
+        await inventoryPage.menuButton.click();
+        await expect(page).toHaveScreenshot('menuOpen.png', { fullPage: true });
+      });
+
+      test('Products added to cart', async ({ page }) => {
+        await inventoryPage.addAllProductsToCart();
+        await expect(page).toHaveScreenshot('productsInCart.png', { fullPage: true });
+      });
+    });
+  });
+
+  test.describe('Product tests', () => {
+    const NUM_PRODUCTS = PRODUCT_INFO.length;
+
+    test('Product styling', async () => {
+      for (let i = 0; i < NUM_PRODUCTS; i++) {
+        await expect(inventoryPage.inventoryItem.nth(i)).toHaveCSS('border', `1px solid ${COLORS.productBorderColor}`);
+        await expect(inventoryPage.inventoryItem.nth(i)).toHaveCSS('border-radius', '8px');
+        await expect(inventoryPage.inventoryItem.nth(i)).toHaveCSS('display', 'flex');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.title)).toHaveCSS(
+          'color',
+          COLORS.productTitleColor
+        );
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.title)).toHaveCSS('font-size', '20px');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.title)).toHaveCSS('font-weight', '500');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.price)).toHaveCSS('font-size', '20px');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.price)).toHaveCSS('font-weight', '500');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button)).toHaveCSS('font-size', '16px');
+        await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button)).toHaveCSS('font-weight', '500');
+      }
+    });
+
+    test.describe('Product details', () => {
+      [
+        {
+          description: 'Default sort (name A-Z)',
+          products: [...PRODUCT_INFO].sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0)),
+          sortBy: 'Name (A to Z)',
+          sortOption: 'default',
+        },
+        {
+          description: 'Sort by name (Z-A)',
+          products: [...PRODUCT_INFO].sort((a, b) => (b.title > a.title ? 1 : a.title > b.title ? -1 : 0)),
+          sortBy: 'Name (Z to A)',
+          sortOption: 'za',
+        },
+        {
+          description: 'Sort by price (low-high)',
+          products: [...PRODUCT_INFO].sort((a, b) => (a.price > b.price ? 1 : b.price > a.price ? -1 : 0)),
+          sortBy: 'Price (low to high)',
+          sortOption: 'lohi',
+        },
+        {
+          description: 'Sort by price (high-low)',
+          products: [...PRODUCT_INFO].sort((a, b) => (b.price > a.price ? 1 : a.price > b.price ? -1 : 0)),
+          sortBy: 'Price (high to low)',
+          sortOption: 'hilo',
+        },
+      ].forEach((testCase) => {
+        test(testCase.description, async () => {
+          if (testCase.sortOption !== 'default') {
+            await inventoryPage.sortSelect.selectOption(testCase.sortOption);
+          }
+          await expect(inventoryPage.activeSortOption).toHaveText(testCase.sortBy);
+
+          for (let i = 0; i < testCase.products.length; i++) {
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.img)).toHaveAttribute(
+              'src',
+              testCase.products[i].imgSrc
+            );
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.img)).toHaveAttribute(
+              'alt',
+              testCase.products[i].title
+            );
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.title)).toHaveText(
+              testCase.products[i].title
+            );
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.description)).toHaveText(
+              testCase.products[i].description
+            );
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.price)).toHaveText(
+              `\$${testCase.products[i].price}`
+            );
+
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button)).toBeVisible();
+            await expect(inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button)).toHaveText(
+              EXPECTED_TEXT.addToCartButton
+            );
+          }
+        });
+      });
+    });
+
+    test.describe('Add products to cart & remove', () => {
+      test('Add product to cart', async () => {
+        const PRODUCT_INDEX = Math.floor(Math.random() * NUM_PRODUCTS);
+        await inventoryPage.getProductElement(PRODUCT_INDEX, PRODUCT_ELEMENTS.button).click();
+
+        // Verify cart
+        await expect(inventoryPage.shoppingCartBadge).toBeVisible();
+        await expect(inventoryPage.shoppingCartBadge).toHaveText('1');
+
+        // Verify button on relevant product has changed correctly
+        await inventoryPage.verifyCartButtonStyle(PRODUCT_INDEX, 'remove');
+
+        // Verify other products unchanged
+        for (let i = 0; i < NUM_PRODUCTS; i++) {
+          if (i !== PRODUCT_INDEX) {
+            await inventoryPage.verifyCartButtonStyle(i, 'add');
+          }
+        }
+      });
+
+      test('Add multiple products to cart', async () => {
+        for (let i = 0; i < NUM_PRODUCTS; i++) {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button).click();
+          await expect(inventoryPage.shoppingCartBadge).toHaveText(`${i + 1}`);
+          await inventoryPage.verifyCartButtonStyle(i, 'remove');
+        }
+      });
+
+      test('Remove only product from cart', async () => {
+        await inventoryPage.getProductElement(0, PRODUCT_ELEMENTS.button).click();
+        await expect(inventoryPage.shoppingCartBadge).toHaveText('1');
+
+        await inventoryPage.getProductElement(0, PRODUCT_ELEMENTS.button).click();
+        await expect(inventoryPage.shoppingCartBadge).toHaveCount(0);
+        await inventoryPage.verifyCartButtonStyle(0, 'add');
+      });
+
+      test('Remove multiple products from cart', async () => {
+        await inventoryPage.addAllProductsToCart();
+        await expect(inventoryPage.shoppingCartBadge).toHaveText(`${NUM_PRODUCTS}`);
+
+        for (let i = 0; i < NUM_PRODUCTS; i++) {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.button).click();
+          if (i === NUM_PRODUCTS - 1) {
+            await expect(inventoryPage.shoppingCartBadge).toHaveCount(0);
+          } else {
+            await expect(inventoryPage.shoppingCartBadge).toHaveText(`${NUM_PRODUCTS - (i + 1)}`);
+          }
+          await inventoryPage.verifyCartButtonStyle(i, 'add');
+        }
+      });
+    });
+
+    test('Only title & img have link to product page', async () => {
+      for (let i = 0; i < NUM_PRODUCTS; i++) {
+        // I don't like using locators within tests but this is the easiest way of conducting this test
+        const LINK = inventoryPage.inventoryItem.nth(i).locator('a');
+        await expect(LINK).toHaveCount(2);
+        // Verifying this ID doesn't really show the link is on the image but it can be implied from the ID
+        // Verifying the link position in the DOM relative to other elements (e.g. the product image) is tricky and offers relatively low value
+        await expect(LINK.nth(0)).toHaveId(new RegExp('item_\\d_img_link'));
+        await expect(LINK.nth(0)).toHaveAttribute('href', '#');
+        await expect(LINK.nth(1)).toHaveId(new RegExp('item_\\d_title_link'));
+        await expect(LINK.nth(1)).toHaveAttribute('href', '#');
+      }
+    });
+
+    for (let i = 0; i < NUM_PRODUCTS; i++) {
+      test.describe(` ${PRODUCT_INFO[i].title} link tests`, () => {
+        test(`Clicking title opens product page`, async ({ page, baseURL }) => {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.title).click();
+          await expect(page).toHaveURL(`${baseURL}/inventory-item.html?id=${PRODUCT_INFO[i].id}`);
+        });
+
+        test(`Clicking image opens product page`, async ({ page, baseURL }) => {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.img).click();
+          await expect(page).toHaveURL(`${baseURL}/inventory-item.html?id=${PRODUCT_INFO[i].id}`);
+        });
+
+        test(`Clicking description does nothing`, async ({ page, baseURL }) => {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.description).click();
+          await expect(page).toHaveURL(`${baseURL}${inventoryPage.url}`);
+        });
+
+        test(`Clicking price does nothing`, async ({ page, baseURL }) => {
+          await inventoryPage.getProductElement(i, PRODUCT_ELEMENTS.price).click();
+          await expect(page).toHaveURL(`${baseURL}${inventoryPage.url}`);
+        });
+      });
+    }
+  });
+
+  test('Shopping cart link opens cart page', async ({ page, baseURL }) => {
+    // The shopping cart link doesn't actually have an href attribute but still opens the cart page
+    await expect(inventoryPage.shoppingCartLink).not.toHaveAttribute('href');
+    await inventoryPage.shoppingCartLink.click();
+    await expect(page).toHaveURL(`${baseURL}/cart.html`);
+  });
+
+  test('Social media links open relevant page in new tab', async () => {
+    for (let i = 0; i < SOCIAL_LINKS.length; i++) {
+      await expect(inventoryPage.socialMediaLink.nth(i)).toHaveAttribute('href', SOCIAL_LINKS[i]);
+      await expect(inventoryPage.socialMediaLink.nth(i)).toHaveAttribute('target', '_blank');
+      await expect(inventoryPage.socialMediaLink.nth(i)).toHaveAttribute('rel', 'noreferrer');
+    }
+  });
+});
