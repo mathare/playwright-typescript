837dca641d5797c08290edf76fbea744d8710611
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-11-14 16:26:50 +0000

    Product page tests (#6)
    * Element visibility test
    
    * Further visibility test
    
    * Verify text content
    
    * Verify element styles
    
    * Visual tests
    
    * Verify links
    
    * Verify back button behaviour
    
    * Fix tests
    
    * Rework tests for common page elements
    
    * Initial per-product tests
    
    * Product visual tests
    
    * Update product info
    
    * Convert visual test to full page snapshot
    
    * Rework button locator
    
    * Verify style of product elements
    
    * Add/remove product to/from cart
    
    * Verify product image attrs
    
    * Add comments to visual tests
    
    * Win snapshots
    
    * Rename snapshot
    
    * Linux snapshots

--- pages/inventoryPage.ts
+++ pages/inventoryPage.ts
@@ -111,6 +111,7 @@ export const PRODUCT_INFO = [
     description:
       'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
     price: 29.99,
+    shortName: 'Backpack',
   },
   {
     id: 0,
@@ -119,6 +120,7 @@ export const PRODUCT_INFO = [
     description:
       "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
     price: 9.99,
+    shortName: 'Bike Light',
   },
   {
     id: 1,
@@ -127,6 +129,7 @@ export const PRODUCT_INFO = [
     description:
       'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
     price: 15.99,
+    shortName: 'Bolt T-Shirt',
   },
   {
     id: 5,
@@ -135,6 +138,7 @@ export const PRODUCT_INFO = [
     description:
       "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
     price: 49.99,
+    shortName: 'Fleece Jacket',
   },
   {
     id: 2,
@@ -143,6 +147,7 @@ export const PRODUCT_INFO = [
     description:
       "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
     price: 7.99,
+    shortName: 'Onesie',
   },
   {
     id: 3,
@@ -151,6 +156,7 @@ export const PRODUCT_INFO = [
     description:
       'This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.',
     price: 15.99,
+    shortName: 'Red T-Shirt',
   },
 ];
 
---
+++ pages/productPage.ts
@@ -0,0 +1,70 @@
+import { expect, Locator, Page } from '@playwright/test';
+import { COLORS, EXPECTED_TEXT } from './inventoryPage';
+
+export class ProductPage {
+  readonly url = '/inventory-item.html?id=';
+  readonly page: Page;
+  readonly body: Locator;
+  readonly headerContainer: Locator;
+  readonly menuButton: Locator;
+  readonly menu: Locator;
+  readonly menuItem: Locator;
+  readonly menuCloseButton: Locator;
+  readonly title: Locator;
+  readonly shoppingCartContainer: Locator;
+  readonly shoppingCartLink: Locator;
+  readonly shoppingCartBadge: Locator;
+  readonly secondaryHeader: Locator;
+  readonly backButton: Locator;
+  readonly inventoryItemContainer: Locator;
+  readonly inventoryItem: Locator;
+  readonly productImage: Locator;
+  readonly productName: Locator;
+  readonly productDescription: Locator;
+  readonly productPrice: Locator;
+  readonly cartButton: Locator;
+  readonly footer: Locator;
+  readonly socialMediaItem: Locator;
+  readonly socialMediaLink: Locator;
+  readonly footerCopy: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.body = page.locator('body');
+    this.headerContainer = page.getByTestId('header-container');
+    this.menuButton = this.headerContainer.locator('#react-burger-menu-btn');
+    this.menu = page.locator('div.bm-menu-wrap');
+    this.menuItem = this.menu.locator('a.menu-item');
+    this.menuCloseButton = this.menu.locator('#react-burger-cross-btn');
+    this.title = this.headerContainer.locator('div.app_logo');
+    this.shoppingCartContainer = this.headerContainer.locator('#shopping_cart_container');
+    this.shoppingCartLink = this.shoppingCartContainer.getByTestId('shopping-cart-link');
+    this.shoppingCartBadge = this.shoppingCartLink.getByTestId('shopping-cart-badge');
+    this.secondaryHeader = page.getByTestId('secondary-header');
+    this.backButton = this.secondaryHeader.getByTestId('back-to-products');
+    this.inventoryItemContainer = page.getByTestId('inventory-container');
+    this.inventoryItem = page.getByTestId('inventory-item');
+    this.productImage = this.inventoryItem.locator('img.inventory_details_img');
+    this.productName = this.inventoryItem.getByTestId('inventory-item-name');
+    this.productDescription = this.inventoryItem.getByTestId('inventory-item-desc');
+    this.productPrice = this.inventoryItem.getByTestId('inventory-item-price');
+    this.cartButton = this.inventoryItem.locator('button');
+    this.footer = page.getByTestId('footer');
+    this.socialMediaItem = this.footer.locator('ul.social li');
+    this.socialMediaLink = this.socialMediaItem.locator('a');
+    this.footerCopy = this.footer.getByTestId('footer-copy');
+  }
+
+  // **********
+  // ASSERTIONS
+  // **********
+  async verifyCartButtonStyle(style: 'add' | 'remove'): Promise<void> {
+    const BUTTON_TEXT = style === 'add' ? EXPECTED_TEXT.addToCartButton : EXPECTED_TEXT.removeButton;
+    const BUTTON_COLOR = style === 'add' ? COLORS.addButtonColor : COLORS.removeButtonColor;
+    const BUTTON_CLASS = style === 'add' ? 'btn_primary' : 'btn_secondary';
+    await expect(this.cartButton).toHaveText(BUTTON_TEXT);
+    await expect(this.cartButton).toContainClass(BUTTON_CLASS);
+    await expect(this.cartButton).toHaveCSS('border', `1px solid ${BUTTON_COLOR}`);
+    await expect(this.cartButton).toHaveCSS('color', BUTTON_COLOR);
+  }
+}
