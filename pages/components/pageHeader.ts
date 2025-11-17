7c50e4b2d902f87530f2e76aa8a28c732483b724
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-11-17 15:56:46 +0000

    Add primaryHeader element

--- pages/components/pageHeader.ts
+++ pages/components/pageHeader.ts
@@ -3,6 +3,7 @@ import { Locator, Page } from '@playwright/test';
 export class PageHeader {
   readonly page: Page;
   readonly headerContainer: Locator;
+  readonly primaryHeader: Locator;
   readonly menuButton: Locator;
   readonly menu: Locator;
   readonly menuItem: Locator;
@@ -16,12 +17,13 @@ export class PageHeader {
   constructor(page: Page) {
     this.page = page;
     this.headerContainer = page.getByTestId('header-container');
-    this.menuButton = this.headerContainer.locator('#react-burger-menu-btn');
+    this.primaryHeader = this.headerContainer.getByTestId('primary-header');
+    this.menuButton = this.primaryHeader.locator('#react-burger-menu-btn');
     this.menu = page.locator('div.bm-menu-wrap');
     this.menuItem = this.menu.locator('a.menu-item');
     this.menuCloseButton = this.menu.locator('#react-burger-cross-btn');
-    this.title = this.headerContainer.locator('div.app_logo');
-    this.shoppingCartContainer = this.headerContainer.locator('#shopping_cart_container');
+    this.title = this.primaryHeader.locator('div.app_logo');
+    this.shoppingCartContainer = this.primaryHeader.locator('#shopping_cart_container');
     this.shoppingCartLink = this.shoppingCartContainer.getByTestId('shopping-cart-link');
     this.shoppingCartBadge = this.shoppingCartLink.getByTestId('shopping-cart-badge');
     this.secondaryHeader = page.getByTestId('secondary-header');
