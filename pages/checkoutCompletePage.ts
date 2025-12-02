8068e390f7691bedd702b4d992a55bab47f8d0a1
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-12-02 15:14:47 +0000

    Checkout complete page tests (#25)
    * Element visibility test
    
    * Text content of elements
    
    * Element styling
    
    * Visual tests
    
    * Button behavioural test
    
    * Win32 snapshots
    
    * Linux snapshots

---
+++ pages/checkoutCompletePage.ts
@@ -0,0 +1,42 @@
+import { Locator, Page } from '@playwright/test';
+import { PageHeader } from './components/pageHeader';
+import { PageFooter } from './components/pageFooter';
+
+export class CheckoutCompletePage {
+  readonly page: Page;
+  readonly pageHeader: PageHeader;
+  readonly subtitle: Locator;
+  readonly checkoutCompleteContainer: Locator;
+  readonly checkoutCompleteImg: Locator;
+  readonly checkoutCompleteHeader: Locator;
+  readonly checkoutCompleteText: Locator;
+  readonly backButton: Locator;
+  readonly pageFooter: PageFooter;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.pageHeader = new PageHeader(page);
+    this.subtitle = page.getByTestId('title');
+    this.checkoutCompleteContainer = page.getByTestId('checkout-complete-container');
+    this.checkoutCompleteImg = this.checkoutCompleteContainer.getByTestId('pony-express');
+    this.checkoutCompleteHeader = this.checkoutCompleteContainer.getByTestId('complete-header');
+    this.checkoutCompleteText = this.checkoutCompleteContainer.getByTestId('complete-text');
+    this.backButton = this.checkoutCompleteContainer.getByTestId('back-to-products');
+    this.pageFooter = new PageFooter(page);
+  }
+}
+
+export const EXPECTED_TEXT = {
+  subtitle: 'Checkout: Complete!',
+  header: 'Thank you for your order!',
+  body: 'Your order has been dispatched, and will arrive just as fast as the pony can get there!',
+  button: 'Back Home',
+};
+
+export const COLORS = {
+  textColor: 'rgb(19, 35, 34)',
+  backButton: {
+    backgroundColor: 'rgb(61, 220, 145)',
+    borderColor: 'rgb(19, 35, 34)',
+  },
+};
