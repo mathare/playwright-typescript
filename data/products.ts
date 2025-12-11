4622036f559fe813b57a72b558140384b6edd8ea
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-12-11 11:48:09 +0000

    Add boolean flag to product data

--- data/products.ts
+++ data/products.ts
@@ -1,4 +1,14 @@
-export const PRODUCT_INFO = [
+type Product = {
+  id: number;
+  title: string;
+  imgSrc: string;
+  description: string;
+  price: number;
+  shortName: string;
+  restrictedPurchase: boolean;
+};
+
+export const PRODUCT_INFO: Product[] = [
   {
     id: 4,
     title: 'Sauce Labs Backpack',
@@ -7,6 +17,7 @@ export const PRODUCT_INFO = [
       'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
     price: 29.99,
     shortName: 'Backpack',
+    restrictedPurchase: false,
   },
   {
     id: 0,
@@ -16,6 +27,7 @@ export const PRODUCT_INFO = [
       "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
     price: 9.99,
     shortName: 'Bike Light',
+    restrictedPurchase: false,
   },
   {
     id: 1,
@@ -25,6 +37,7 @@ export const PRODUCT_INFO = [
       'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
     price: 15.99,
     shortName: 'Bolt T-Shirt',
+    restrictedPurchase: true,
   },
   {
     id: 5,
@@ -34,6 +47,7 @@ export const PRODUCT_INFO = [
       "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
     price: 49.99,
     shortName: 'Fleece Jacket',
+    restrictedPurchase: true,
   },
   {
     id: 2,
@@ -43,6 +57,7 @@ export const PRODUCT_INFO = [
       "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
     price: 7.99,
     shortName: 'Onesie',
+    restrictedPurchase: false,
   },
   {
     id: 3,
@@ -52,5 +67,6 @@ export const PRODUCT_INFO = [
       'This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.',
     price: 15.99,
     shortName: 'Red T-Shirt',
+    restrictedPurchase: true,
   },
 ];
